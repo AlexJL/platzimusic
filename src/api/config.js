9fb7711abb1c48f76729affdc1bcf25c70f9ddad
Jhon Leon <aleon.ortecho@gmail.com>
2017-12-14 14:53:44 -0500

    Ininitial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+//Application name 	platzimusic
+//API key 	ce8e4d05d03e63b6eea703e1cdf7542c
+//Shared secret 	003bf37f11b4ade438a113b6a92d9606
+//Registered to 	alexjl13
+
+export default {
+    appName: 'platzimusic',
+    apiKey: 'ce8e4d05d03e63b6eea703e1cdf7542c',
+    secret: '003bf37f11b4ade438a113b6a92d9606',
+    registeredTo: 'alexjl13'
+  }
